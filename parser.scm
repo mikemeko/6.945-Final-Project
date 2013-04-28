ad859d41b8e2cfacf35a170929158e4d5404636d
Max Nelson <maxnelso@Max-Nelsons-MacBook-Pro.local>
2013-04-27 23:50:32 -0400

    wops bugs

--- parser.scm
+++ parser.scm
@@ -1,6 +1,7 @@
 ;;; Returns true if the argument is a the beginning of a tag, a la <p> or <div>
 (define (start-tag? str)
     (and (string-prefix? "<" str)
+         (not (string-suffix? "/>" str))
          (not (eq? #\/ (string-ref str 1)))))
 
 ;;; Returns true if the argument is a closing tag, a la </p> or </div>
@@ -10,11 +11,12 @@
 ;;; Returns true if the argument is a self-terminating tag, a la <a
 ;;; href="http://groups.csail.mit.edu/mac/users/gjs/6.945/"/>
 (define (self-terminating-tag? str)
-    (and (start-tag? str)
+    (and (string-prefix? "<" str)
          (string-suffix? "/>" str)))
 
 ;;; Takes in a list of tokens and parses them, returning a tree
 (define (parser tokens)
-    (define (parser-helper tokens)
+    (define (parser-helper tokens stack tree)
         #t)
-    (parser-helper tokens))
+
+    (parser-helper tokens '() '()))

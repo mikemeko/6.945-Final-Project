6d9cb5c1a3aca6b0bfdfffc10a4214c67afb44ca
Max Nelson <maxnelso@Max-Nelsons-MacBook-Pro.local>
2013-04-27 23:34:55 -0400

    helper functions for classifying nodes

--- parser.scm
+++ parser.scm
@@ -1,12 +1,20 @@
 ;;; Returns true if the argument is a the beginning of a tag, a la <p> or <div>
 (define (start-tag? str)
-    (string=? "#\<" (string-ref str 0)))
+    (and (string-prefix? "<" str)
+         (not (eq? #\/ (string-ref str 1)))))
 
+;;; Returns true if the argument is a closing tag, a la </p> or </div>
+(define (end-tag? str)
+    (string-prefix? "</" str))
+
+;;; Returns true if the argument is a self-terminating tag, a la <a
+;;; href="http://groups.csail.mit.edu/mac/users/gjs/6.945/"/>
+(define (self-terminating-tag? str)
+    (and (start-tag? str)
+         (string-suffix? "/>" str)))
 
 ;;; Takes in a list of tokens and parses them, returning a tree
 (define (parser tokens)
     (define (parser-helper tokens)
         #t)
-
-
     (parser-helper tokens))

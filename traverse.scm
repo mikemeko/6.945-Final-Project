b5e7b4ba29e64603414069c1a3d337b992d9f380
Paul Woods <pwoods@mit.edu>
2013-04-28 23:12:45 -0400

    removes procedures defined via define-record-type from traverse and adds code used to test record mutability behaves as expected

--- traverse.scm
+++ traverse.scm
@@ -17,37 +17,3 @@
 
 )
 
-; METHODS INDEPENDENT OF TREE STRUCTURE
-
-; takes in a given node
-; returns type of node
-(define get-type (node)
-
-  ; wrap around define-structure
-
-)
-
-; takes in node and type
-; replaces node's current type with new type
-(define set-type (node type)
-
-  ; wrap around define-structure
-
-)
-
-; takes in a given node
-; returns list of attributes of node
-(define get-attr (node)
-
-  ; wrap around define-structure
-  
-)
-
-; takes in node and list of attributes in form ("..", "..")
-; replaces current attributes of node with new list
-(define set-attr (node attrs)
-
-  ; wrap around define-structure
-
-)
-

07ac181d3a00f1c98a8220e34fab83891f4939d9
Max Nelson <maxnelso@Max-Nelsons-MacBook-Pro.local>
2013-04-29 00:16:54 -0400

    adding stripping tags

--- tokenize.scm
+++ tokenize.scm
@@ -1,3 +1,12 @@
+
+;;; Strips off the auxillary strings of a tag (including the outer brackets/slashes
+;;; Assumes the tag is of valid XML syntax
+(define (get-element tag)
+    (let ((left (if (string-prefix? "</" tag) 2 1))
+          (right (if (string-suffix? "/>" tag) (- (string-length tag) 2) (- (string-length tag) 1))))
+        (substring tag left right)))
+
+
 ;; XML Tokenizer
 
 ;;; tokenizes the given |text| into tokens, where a token is either a block of

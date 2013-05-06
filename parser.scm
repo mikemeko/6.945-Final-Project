990b7f91c9d34a09c3450bd225aaf8675a2b47ab
Max Nelson <maxnelso@Max-Nelsons-MacBook-Pro.local>
2013-05-05 22:17:55 -0400

    fixing add-child nonsense

--- parser.scm
+++ parser.scm
@@ -1,22 +1,23 @@
-
 ;;; TODO(mikemeko): comment
 (define (make-tree start? end? create-node-from-token)
     (define (make-tree-helper stack tokens)
         (if (= (length tokens) 0)
-            (car stack) ;TODO (marx) make sure stack has one element
+            (begin 
+            (assert (= (length stack) 1) "Stack does not have one element!")
+            (car stack))
             (let ((first-token (car tokens)))
                 (cond ((start? first-token)
                             (let ((new-node (new-segment (create-node-from-token first-token) '())))
                                   (make-tree-helper (append (list new-node) stack) (cdr tokens))))
                       ((end? first-token)
-                            (let ((first-subtree (car stack));TODO (marx) add assertions for matching tag
+                            (let ((first-subtree (car stack))
                                    (second-subtree (cadr stack)))
-                                       (begin (add-child second-subtree first-subtree)
-                                            (make-tree-helper (append (list second-subtree) (cddr stack)) (cdr tokens)))))
+                                            (add-child second-subtree first-subtree)
+                                            (make-tree-helper (append (list second-subtree) (cddr stack)) (cdr tokens))))
                       (else
                             (let ((new-node (new-segment (create-node-from-token first-token) '()))
                                    (first-subtree (car stack)))
-                                       (begin (set-children first-subtree (append (children first-subtree) (list new-node)))
+                                       (begin (add-child first-subtree new-node)
                                         (make-tree-helper (append (list first-subtree) (cdr stack)) (cdr tokens)))))))))
     (let ((root (make-node '*the-root* '())))
         (lambda (tokens)
